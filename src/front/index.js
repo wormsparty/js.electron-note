1aca0dddf1d7f07ebdd9288fbcaf60ab2cdbdb70
wormsparty <wormsparty@gmail.com>
2023-12-22 15:17:55 +0100

    Some sort of menu

--- src/front/index.js
+++ src/front/index.js
@@ -3,3 +3,18 @@ async function clic() {
 	console.log('Files = ');
 	console.log(files);
 }
+
+function draw() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.fill();
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	draw();
+});
