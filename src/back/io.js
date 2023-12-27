996443c45925f366ea5e0fbab07a6aa17264ded8
wormsparty <wormsparty@gmail.com>
2023-12-27 02:31:28 +0100

    Linked maps

--- src/back/io.js
+++ src/back/io.js
@@ -7,28 +7,8 @@ exports.load = (name) => {
 	try {
 		return JSON.parse(fs.readFileSync(`data/${name}`, { encoding: 'utf8', flag: 'r' }));
 	} catch (error) {
-		if (name !== 'default.json') {
-			console.log(error);
-			return null;
-		}
-		
-		const mapData = {
-			width: 64, 
-			height: 24,
-			currentX: 32,
-			currentY: 12,
-			map: [],
-			color: [],
-			neighbor: [null, null, null, null],
-		}
-
-		for (let i = 0; i < mapData.width * mapData.height; i++) {
-			// Put gray dots by default
-			mapData.map[i] = '.';
-			mapData.color[i] = 1;
-		}
-		
-		return mapData;
+		console.log(error);
+		return null;		
 	}
 };
 
--- src/front/index.js
+++ src/front/index.js
@@ -117,6 +117,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	consoleFont.load().then(async (font) => {
 		document.fonts.add(font);
 		grid = await window.api.load('default.json');
+
+		if (grid === null) {
+			grid = newMap('default.json');
+		}
+
 		onresize();
 	});
 });
@@ -149,9 +154,83 @@ const moveRight = () => {
 	}
 };
 
-const write = (chr) => {
-	console.log(chr);
+const moveDown = () => {
+	if (grid.currentY < grid.height - 1) {
+		grid.currentY++;
+	}
+};
 
+const newMap = (name) => {
+	const mapData = {
+		width: 64, 
+		height: 24,
+		currentX: 32,
+		currentY: 12,
+		map: [],
+		color: [],
+		neighbors: [null, null, null, null],
+		name: name,
+	}
+
+	for (let i = 0; i < mapData.width * mapData.height; i++) {
+		// Put gray dots by default
+		mapData.map[i] = '.';
+		mapData.color[i] = 1;
+	}
+	
+	console.log('Map ' + name + ' created');
+	return mapData;
+}
+
+const makeId = (length) => {
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const charactersLength = characters.length;
+	let counter = 0;
+	
+	while (counter < length) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		counter += 1;
+	}
+
+	return result;
+}
+
+const goTo = async (index) => {
+	let newGrid;
+	const map = grid.neighbors[index];
+
+	if (map === null) {
+		// TODO: Be able to rename
+		const newName = `${makeId(8)}.json`;
+		grid.neighbors[index] = newName;
+		window.api.save(grid);
+
+		newGrid = newMap(newName);
+
+		if (index === 0) {
+			newGrid.neighbors[3] = grid.name;
+		} else if (index === 1) {
+			newGrid.neighbors[2] = grid.name;
+		} else if (index === 2) {
+			newGrid.neighbors[1] = grid.name;
+		} else {
+			newGrid.neighbors[0] = grid.name;
+		}
+
+		window.api.save(newGrid);
+	} else {
+		newGrid = await window.api.load(map);
+		console.log('Loaded ' + map);
+	}
+
+	if (newGrid !== null) {
+		grid = newGrid;
+		onresize();
+	}
+};
+
+const write = (chr) => {
 	grid.map[grid.currentX + grid.currentY * grid.width] = chr;
 	grid.color[grid.currentX + grid.currentY * grid.width] = currentColor;
 };
@@ -164,9 +243,7 @@ window.addEventListener("keydown", async (event) => {
 			grid.currentY--;
 		}
 	} else if (event.key === 'ArrowDown') {
-		if (grid.currentY < grid.height - 1) {
-			grid.currentY++;
-		}
+		moveDown();
 	} else if (event.key === 'ArrowLeft') {
 		moveLeft();
 	} else if (event.key === 'ArrowRight') {
@@ -177,12 +254,22 @@ window.addEventListener("keydown", async (event) => {
 		currentColor = 1;
 		write(' ');
 		currentColor = color;
-	} else if (event.key === 'Delete') {
+	} else if (event.key === 'Enter') {
+		moveDown();
+	} else if (event.key === ' ') {
 		const color = currentColor;
 		currentColor = 1;
 		write(' ');
 		currentColor = color;
 		moveRight();
+	} else if (event.key === 'Delete') {
+		goTo(0);
+	} else if (event.key === 'Home') {
+		goTo(1);
+	} else if (event.key === 'End') {
+		goTo(2);
+	} else if (event.key === 'PageDown') {
+		goTo(3);
 	} else if (event.key >= '1' && event.key <= '9') {
 		const newColor = colors[event.key];
 
