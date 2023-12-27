97d563cd2e819707964fa656b4bfd6586247f470
wormsparty <wormsparty@gmail.com>
2023-12-27 11:15:27 +0100

    Split consts

---
+++ src/front/consts.js
@@ -0,0 +1,20 @@
+const colors = [
+  '#FFFFFF', /* white */
+  '#646464', /* gray */
+  '#EF2929', /* red */
+  '#8AE234', /* green */
+  '#FCE94F', /* yellow */
+  '#0000FF', /* blue */
+  '#AD7fA8', /* magenta */
+  '#AA3300', /* brown */
+  '#050505', /* black */
+];
+
+// TODO !
+//  -> Map connections
+//  -> Color by character and/or layer
+const items = '*$()[]%&/?!';
+const map = '<>^v+/';
+const characters = '@t';
+const obstacles = '#~.*';
+
--- src/front/index.js
+++ src/front/index.js
@@ -2,26 +2,6 @@
 // See backend folder for its definition.
 let grid = {};
 
-const colors = [
-  '#FFFFFF', /* white */
-  '#646464', /* gray */
-  '#EF2929', /* red */
-  '#8AE234', /* green */
-  '#FCE94F', /* yellow */
-  '#0000FF', /* blue */
-  '#AD7fA8', /* magenta */
-  '#AA3300', /* brown */
-  '#050505', /* black */
-];
-
-// TODO !
-//  -> Map connections
-//  -> Color by character and/or layer
-const items = '*$()[]%&/?!';
-const map = '<>^v+/';
-const characters = '@t';
-const obstacles = '#~.*';
-
 let currentColor = 0;
 let mode = 'edit';
 let question = '';
